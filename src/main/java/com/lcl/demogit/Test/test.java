3aa83279605a6391d95d765a93f6ee84186d04dd
lcl <2500433927@qq.com>
2022-05-11 20:06:53 +0800

    1

---
+++ src/main/java/com/lcl/demogit/Test/test.java
@@ -0,0 +1,11 @@
+package com.lcl.demogit.Test;
+
+/**
+ * @author shkstart
+ * @create 2022-05-11 19:55
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("first");
+    }
+}
